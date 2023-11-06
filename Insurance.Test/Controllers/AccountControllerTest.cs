4b7ccceda2dcaf931cf453f58a44c5d9eaa419d4
ebelebe sheriff <sheriff.ebelebe@gmail.com>
2023-11-06 14:46:34 +0100

    -- added mock request data for testing login

--- Insurance.Test/Controllers/AccountControllerTest.cs
+++ Insurance.Test/Controllers/AccountControllerTest.cs
@@ -1,4 +1,6 @@
-﻿using InsuranceAPI.Controllers;
+﻿using InfrastructureLayer.IServices;
+using InsuranceAPI.Controllers;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,20 @@ namespace Insurance.Test.Controllers
     {
 
         private readonly AccountController _sut;
+        private readonly Mock<IAccountService> _mockAccountService;
 
+        public AccountControllerTest()
+        {
+            _mockAccountService = new();
+            _sut = new(_mockAccountService.Object);
+        }
+
+
+        [Fact]
+        public void Login_Should_Return_BadRequest()
+        {
+
+        }
 
     }
 }
---
+++ Insurance.Test/MockData/MockDataClass.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using DataAccessLayer.Model.Dto.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance.Test.MockData
+{
+    public class MockDataClass
+    {
+
+        public List<ValidateUserRequest> GenerateLoginRequestObject()
+        {
+            var req = new Faker<ValidateUserRequest>()
+                .RuleFor(x => x.Username, x => x.Person.Email)
+                .RuleFor(x => x.Password, x => x.Random.AlphaNumeric(10))
+                .RuleFor(x => x.Role, x => x.Name.JobTitle());
+
+            return req.Generate(1);
+        }
+    }
+}
