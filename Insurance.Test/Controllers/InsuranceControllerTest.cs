fc8e693d622f047f970ce452953b8a654e46ddf2
ebelebe sheriff <sheriff.ebelebe@gmail.com>
2023-11-06 20:52:07 +0100

    --added unit test for process claims should return record not found

--- Insurance.Test/Controllers/InsuranceControllerTest.cs
+++ Insurance.Test/Controllers/InsuranceControllerTest.cs
@@ -182,6 +182,38 @@ namespace Insurance.Test.Controllers
         }
 
 
+        [Fact]
+        public async void ProcessClaims_Should_Return_RecordNotFound()
+        {
+            //Arrange
+            var request = new ProcessClaimRequest()
+            {
+                Id = 1,
+                ProcessTypeId = 1
 
+            };
+
+            var response = new WebApiResponse()
+            {
+                StatusCode = "404",
+
+            };
+            //Assign
+            _mockinsuranceservice.Setup(x => x.ProcessClaims(request)).ReturnsAsync(response);
+            var result = await _sut.ProcessClaims(request);
+
+            result.Should().BeOfType<ObjectResult>();
+
+            var objectResult = (ObjectResult)result;
+            objectResult.StatusCode.Should().Be(404);
+
+            var value = (WebApiResponse?)objectResult.Value;
+
+            value.Should().NotBeNull();
+            value.Should().BeEquivalentTo(response, x => x.ComparingByMembers<WebApiResponse>());
+            value.Should().BeOfType<WebApiResponse>();
+
+            value?.StatusCode.Should().Be("404");
+        }
     }
 }
